6bf4a60a2fa79e402de878781308219fc12428fe
PF <xiaofei0127@outlook.com>
2017-02-13 14:16:47 +0800

    commit
    commit

---
+++ SafeDispatch/SafeDispatch/SafeDispatch.swift
@@ -0,0 +1,38 @@
+//
+//  SafeDispatch.swift
+//  SafeDispatch
+//
+//  Created by PF on 17/2/13.
+//  Copyright © 2017年 PF. All rights reserved.
+//
+
+import UIKit
+
+final public class SafeDispatch {
+    
+    private let mainQueueKey = DispatchSpecificKey<Int>()
+    private let mainQueueValue = Int(1)
+    
+    private static let sharedSafeDispatch = SafeDispatch()
+    
+    private init() {
+        
+        DispatchQueue.main.setSpecific(key: mainQueueKey, value: mainQueueValue)
+    }
+    
+    public class func async(onQueue queue: DispatchQueue = DispatchQueue.main, forWork block: @escaping () -> ()) {
+        if queue == DispatchQueue.main {
+            if DispatchQueue.getSpecific(key: sharedSafeDispatch.mainQueueKey) == sharedSafeDispatch.mainQueueValue {
+                block()
+            } else {
+                DispatchQueue.main.async {
+                    block()
+                }
+            }
+        } else {
+            queue.async {
+                block()
+            }
+        }
+    }
+}
---
+++ SafeDispatch/SafeDispatch/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SafeDispatch
+//
+//  Created by PF on 17/2/13.
+//  Copyright © 2017年 PF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        SafeDispatch.async { 
+            print("task \(Thread.current)")
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
